2972e3b9af68d4230ed945241eed34d91c64c236
anpok28 <68907665+anpok28@users.noreply.github.com>
2020-07-30 10:33:14 -0300

    Update category-info.js

--- js/category-info.js
+++ js/category-info.js
@@ -42,7 +42,7 @@ document.addEventListener("DOMContentLoaded", function(e){
             showImagesGallery(category.images);
 
             console.log("Debajo de esta linea escriban su nombre: ");
-            console.log("Agustin Tosar");
+            console.log("Andrés Aranda");
         }
     });
 });
