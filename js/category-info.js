181f22d90ef5afbc679bc8af7cdd015d8f80cff1
Agustin Tosar <tosar.agustin@gmail.com>
2020-07-30 07:48:08 -0300

    Correccion de nombres

--- js/category-info.js
+++ js/category-info.js
@@ -42,7 +42,6 @@ document.addEventListener("DOMContentLoaded", function(e){
             showImagesGallery(category.images);
 
             console.log("Debajo de esta linea escriban su nombre: ");
-            console.log("mi nombre");
 
 
         }
