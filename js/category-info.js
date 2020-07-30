bb8c6b4cce592af1d39242906b80aca8974c3252
anpok28 <68907665+anpok28@users.noreply.github.com>
2020-07-30 10:47:13 -0300

    Hackeo Agregue mi Nombre
    Agregue por console.log mi nombre sin borrar el anterior

--- js/category-info.js
+++ js/category-info.js
@@ -43,6 +43,7 @@ document.addEventListener("DOMContentLoaded", function(e){
 
             console.log("Debajo de esta linea escriban su nombre: ");
             console.log("Agustin Tosar");
+            console.log("Andrés Aranda");
         }
     });
 });
